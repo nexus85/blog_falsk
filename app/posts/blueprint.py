a3ea0639611faca61564bf8722b55c9289bdb7a1
nexus <mx38@mail.ru>
2020-11-28 00:16:03 +0300

    5

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,6 +1,9 @@
 # кyсок изолированной информации - blueprintd
-
-
+from flask import render_template
 from flask import Blueprint
+posts = Blueprint('posts', __name__, template_folder='templates')  #posts это имя под которым зарегин blueprint
 
-posts = Blueprint('posts', __name__, template_folder='templates')
+@posts.route('/')
+def index():
+
+    return render_template('posts/index.html')
