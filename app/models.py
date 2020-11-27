fbb991c65edaba0865a5b4eda33397d91ca484ed
nexus <mx38@mail.ru>
2020-11-28 02:09:21 +0300

    sleep

--- app/models.py
+++ app/models.py
@@ -1,4 +1,10 @@
 from app import db
+from datetime import datetime
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Stirng(140))
+    stug = db.Column(db.Sting(140), unique=True)
+    body = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.now())
+    
